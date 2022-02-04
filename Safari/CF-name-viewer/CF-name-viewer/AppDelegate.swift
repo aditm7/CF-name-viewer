2d8d17abd89d0e96a1bf161c79ff9722fbbd71e3
Adit Malhotra <aditmalhotra8@gmail.com>
2022-02-04 15:24:07 +0530

    Adding support for Safari browser

---
+++ Safari/CF-name-viewer/CF-name-viewer/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  CF-name-viewer
+//
+//  Created by ADIT MALHOTRA on 04/02/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
